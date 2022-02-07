230debf97b0455f618195790e7ba72987da65af8
Mashariq Khan <mashariq@outlook.com>
2022-02-06 19:27:47 -0600

    Initial commit

---
+++ src/main/java/com/cf/urld/UrldApplication.java
@@ -0,0 +1,13 @@
+package com.cf.urld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cf/urld/UrldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.urld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
